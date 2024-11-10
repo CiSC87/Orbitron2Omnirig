e8050570fc0624ceb5219ad98b78de4059b5be31
unknown <Francesco>
2024-11-10 21:24:51 +0100

    Adjust DL frequency as soon as needed

--- main.py
+++ main.py
@@ -22,18 +22,19 @@ DATA_MAP = [
         ("timestamp_utc",' TU(\\S*)[ |\r\n]'),      # YYYYMMDDhhmmss
         ("timestamp_local",' TL(\\S*)[ |\r\n]'),    # YYYYMMDDhhmmss
 ]
-COMPASS=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+COMPASS = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE",
+           "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
 OMNIRIG_VFOA = 2048
 OMNIRIG_VFOB = 4096
 OMNIRIG_SPLITON = 32768
 OMNIRIG_SPLITOFF = 65536
 OMNIRIG_MODEFM = 1073741824
-FREQ_MAX_DELTA=500
+FREQ_MAX_DELTA = 200
 
 # Global variables :)
-FREQ_CURR_DL=0
-FREQ_CURR_UL=0
-RIG=False
+FREQ_CURR_DL = 0
+FREQ_CURR_UL = 0
+RIG = False
 
 
 def rig_init():
@@ -51,7 +52,7 @@ def rig_get_data():
     global RIG
     rig_type = RIG.Rig1.RigType
     rig_status = RIG.Rig1.StatusStr
-    rig_data = {"rig_type": rig_type, "rig_status":rig_status }
+    rig_data = {"rig_type": rig_type, "rig_status": rig_status}
     return rig_data
 
 
@@ -63,72 +64,81 @@ def rig_set_freq(vfo, freq):
 
 
 def deg_to_compass(angle):
-    val=int((angle/22.5)+.5)
+    val = int((angle/22.5)+.5)
     return COMPASS[(val % 16)]
 
 
 def adjust_freq(sat_data):
     global FREQ_CURR_DL
     global FREQ_CURR_UL
-    exact_dl=float(sat_data["sat_downlink"])
-    ecact_ul=float(sat_data["sat_uplink"])
-    if abs(exact_dl-FREQ_CURR_DL)>FREQ_MAX_DELTA:
-        rig_set_freq(vfo=OMNIRIG_VFOA,freq=exact_dl)
-        FREQ_CURR_DL=exact_dl
+    exact_dl = float(sat_data["sat_downlink"])
+    ecact_ul = float(sat_data["sat_uplink"])
+    # Adjust DL freq immediately as there is
+    # no penalty in adjusting VFO A
+    if exact_dl != FREQ_CURR_DL:
+        rig_set_freq(vfo=OMNIRIG_VFOA, freq=exact_dl)
+        FREQ_CURR_DL = exact_dl
         print("Adjusting DL")
-    if abs(ecact_ul-FREQ_CURR_UL)>FREQ_MAX_DELTA:
-        rig_set_freq(vfo=OMNIRIG_VFOB,freq=ecact_ul)
-        FREQ_CURR_UL=ecact_ul
+    # Adjust UL freq if it's off substantially
+    # as we need to switch to VFO B to change it
+    if abs(ecact_ul-FREQ_CURR_UL) > FREQ_MAX_DELTA:
+        rig_set_freq(vfo=OMNIRIG_VFOB, freq=ecact_ul)
+        FREQ_CURR_UL = ecact_ul
         print("Adjusting UL")
 
 
 def handle_data(sat_data_raw, rig_data):
-    sat_data=parse_sat_data(sat_data_raw)
+    sat_data = parse_sat_data(sat_data_raw)
     display_data(sat_data, rig_data)
     adjust_freq(sat_data)
 
 
 def parse_sat_data(data):
-    parsed_dict={}
+    parsed_dict = {}
     for value in DATA_MAP:
         try:
-            parsed_dict[value[0]] = re.findall(value[1],data)[0]
+            parsed_dict[value[0]] = re.findall(value[1], data)[0]
         except Exception:
             parsed_dict[value[0]] = False
-    return(parsed_dict)
+    return (parsed_dict)
 
 
 def display_data(sat_data, rig_data):
     os.system('cls')
     try:
-        timestamp_local = datetime.datetime.strptime(sat_data['timestamp_local'],'%Y%m%d%H%M%S')
-        timestamp_utc = datetime.datetime.strptime(sat_data['timestamp_utc'],'%Y%m%d%H%M%S')
+        timestamp_local = datetime.datetime.strptime(
+            sat_data['timestamp_local'], '%Y%m%d%H%M%S')
+        timestamp_utc = datetime.datetime.strptime(
+            sat_data['timestamp_utc'], '%Y%m%d%H%M%S')
         print("Timestamp :", timestamp_local, "local time")
-        print("Timestamp :", timestamp_utc,"UTC")
+        print("Timestamp :", timestamp_utc, "UTC")
         print("Radio     :", rig_data['rig_type'], end=' ')
         if rig_data['rig_status'] != "On-line":
-            print(colored("off-line","red"))
+            print(colored("off-line", "red"))
         else:
             print("on-line")
         print("Satellite :", sat_data['sat_name'])
-        print("Obs. PoV sat. Elevation  :",int(float(sat_data['sat_elevation'])),"deg",end='')
-        if float(sat_data['sat_elevation'])>0:
+        print("Obs. PoV sat. Elevation  :", int(
+            float(sat_data['sat_elevation'])), "deg", end='')
+        if float(sat_data['sat_elevation']) > 0:
             print(" (above the horizon, visible)")
         else:
-            print(colored(" (below the horizon, not visible)","red"))
-        print("Obs. PoV sat. Azimuth    :",int(float(sat_data['sat_azimuth'])),"deg",end=' ')
+            print(colored(" (below the horizon, not visible)", "red"))
+        print("Obs. PoV sat. Azimuth    :", int(
+            float(sat_data['sat_azimuth'])), "deg", end=' ')
         print("("+deg_to_compass(float(sat_data['sat_azimuth']))+")")
-        print("Obs. PoV sat. Distance   :",int(float(sat_data['sat_distance'])),"km")
-        print("Obs. PoV sat. Velocity   :",int(float(sat_data['sat_velocity'])*60*60),"km/h",end='')
-        if float(sat_data['sat_velocity'])<0:
+        print("Obs. PoV sat. Distance   :", int(
+            float(sat_data['sat_distance'])), "km")
+        print("Obs. PoV sat. Velocity   :", int(
+            float(sat_data['sat_velocity'])*60*60), "km/h", end='')
+        if float(sat_data['sat_velocity']) < 0:
             print(" (approaching)")
         else:
             print(" (going away)")
-        print("Obs. PoV sat. Freq. up   :",sat_data['sat_uplink'],"Hz")
-        print("Obs. PoV sat. Freq. down :",sat_data['sat_downlink'],"Hz")
+        print("Obs. PoV sat. Freq. up   :", sat_data['sat_uplink'], "Hz")
+        print("Obs. PoV sat. Freq. down :", sat_data['sat_downlink'], "Hz")
     except Exception:
         print("Broken data!")
-
 
 
 if __name__ == "__main__":
@@ -146,7 +156,8 @@ if __name__ == "__main__":
                     rig_init()
                     break
                 except Exception as e:
-                    print("DDE conversation error: ", e, " - Retrying in 1 sec...")
+                    print("DDE conversation error: ",
+                          e, " - Retrying in 1 sec...")
                     time.sleep(1)
             # Handle data loop
             while True:
@@ -154,7 +165,7 @@ if __name__ == "__main__":
                 rig_data = rig_get_data()
                 handle_data(sat_data_raw, rig_data)
                 time.sleep(1)
-        
+
         except KeyboardInterrupt:
             sys.exit(0)
         except Exception as e:
